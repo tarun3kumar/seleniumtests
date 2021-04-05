b2aeba224a9170239c5e7c4d17178c2249ba25b3
Tarun Kumar <tarun.kumar@verimi.com>
2021-04-05 14:08:38 +0200

    Update appium client Add Android web test

---
+++ src/test/java/com/seleniumtests/tests/mobile/AndroidWebTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015 www.seleniumtests.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.seleniumtests.tests.mobile;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import com.seleniumtests.core.SeleniumTestPlan;
+import com.seleniumtests.driver.WebUIDriver;
+import com.seleniumtests.webpage.RegistrationPage;
+
+/**
+ * Android app test suite.
+ */
+public class AndroidWebTest extends SeleniumTestPlan {
+
+    /**
+     * Adds two numbers using calculator program.
+     */
+    @Test(groups = {"addTwoNumbers"}, description = "Adds two numbers using calculator program")
+    public void testMobileWeb() throws Exception {
+        new RegistrationPage(true);
+        WebUIDriver.getWebDriver().get("http://www.seleniumtests.com/");
+
+        // close newsletter subscription window if present
+        if (WebUIDriver.getWebDriver().findElements(By.cssSelector(".mc-closeModal")).size() > 0) {
+            WebUIDriver.getWebDriver().findElement(By.cssSelector(".mc-closeModal")).click();
+        }
+
+        new Select(WebUIDriver.getWebDriver().findElement(By.id("PageList1_select"))).selectByVisibleText("WebDriver Forum");
+        assert WebUIDriver.getWebDriver().findElement(By.cssSelector(".post-title")).getText().equals("WebDriver Forum") : "unable to launch Webdriver forum";
+    }
+
+}
